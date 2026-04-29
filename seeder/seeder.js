cccf40479b5e18ba9b185bd53cfebe44eec626b3
Ironboy <ironboy@users.noreply.github.com>
2026-04-29 11:33:03 +0200

    seeder working for creating petowners

--- seeder/seeder.js
+++ seeder/seeder.js
@@ -6,7 +6,7 @@ import petSpecies from './data/petSpecies.json' with {type: 'json'};
 // config/settings
 // note: prefer 127.0.0.1 before localhost
 // because node.js resolves fetches more reliable to 127.0.0.1
-const NUMBER_OF_PETS_OWNERS = 5; // max 200
+const NUMBER_OF_PET_OWNERS = 5; // max 200
 const NUMBER_OF_PETS = 10; // max 500
 const PETS_WITHOUT_OWNERS_PERCENT = 15;
 const MAX_PETS_PER_OWNER = 4;
@@ -25,7 +25,6 @@ function shuffleArray(array) {
   }
 }
 
-
 function generatePetData() {
   // Build weighted species array 
   let speciesList = '';
@@ -33,7 +32,38 @@ function generatePetData() {
     speciesList += (species + ',').repeat(percent);
   }
   speciesList = speciesList.split(',').slice(0, -1);
-
+  // Shuffle - ensure random order of pet names and species list
+  shuffleArray(petNames);
+  shuffleArray(speciesList);
+  // Build the array of pets
+  let pets = [];
+  for (let i = 0; i < NUMBER_OF_PETS; i++) {
+    pets.push({
+      name: petNames.shift(),
+      species: randomItemFromArray(speciesList)
+    });
+  }
+  return pets;
 }
 
-console.log(generatePetData().length);
+async function createPetOwners() {
+  // randomize pet owner order/selection
+  shuffleArray(petOwners);
+  // Write each owner to strapi in a loop
+  for (let i = 0; i < NUMBER_OF_PET_OWNERS; i++) {
+    let response = await fetch(
+      `http://${STRAPI_HOST}:${STRAPI_PORT}/api/pet-owner`,
+      {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ data: petOwners[i] })
+      }
+    );
+    let status = await response.json();
+    console.log(`Creating pet owner ${i + 1}/${NUMBER_OF_PET_OWNERS}`);
+    console.log('Answer from Strapi', status);
+    console.log('');
+  }
+}
+
+createPetOwners();
