2fff115eb528b1dfcd8eab96349f263c70064c4e
Ironboy <ironboy@users.noreply.github.com>
2026-05-04 16:15:57 +0200

    UpdatePetOwner working with strapi

--- src/pages/UpdatePetOwner.jsx
+++ src/pages/UpdatePetOwner.jsx
@@ -3,51 +3,49 @@ import { useNavigate, useParams } from 'react-router';
 import useFetch from '../utils/useFetch';
 
 UpdatePetOwner.route = {
-  path: '/update-owner/:id'
-}
+  path: '/update-owner/:documentId'
+};
 
 export default function UpdatePetOwner() {
 
-  const { id } = useParams()
+  const { documentId } = useParams();
 
   const [formData, setFormData] = useState({
-    id: '',
-    name: '',
+    firstName: '',
+    lastName: '',
     email: ''
-  })
+  });
 
-  const [formSent, setFormSent] = useState(false)
-  const navigate = useNavigate()
+  const [formSent, setFormSent] = useState(false);
+  const navigate = useNavigate();
 
-  const [petOwner, loading] = useFetch('/api/petOwners/' + id)
+  const [petOwnerDataAndMeta, loading] = useFetch('/api/pet-owners/' + documentId);
+  const petOwner = petOwnerDataAndMeta?.data;
 
   useEffect(() => {
-    if (!loading && petOwner) {
-      setFormData({
-        id: petOwner.id ?? '',
-        name: petOwner.name ?? '',
-        email: petOwner.email ?? ''
-      })
+    if (!loading && petOwnerDataAndMeta) {
+      const { firstName, lastName, email } = petOwner;
+      setFormData({ firstName, lastName, email });
     }
-  }, [loading, petOwner])
+  }, [loading, petOwner]);
 
   // order of where this appears is important; after all hooks
-  if (loading) return <p>Loading...</p>
+  if (loading) return <p>Loading...</p>;
 
   function updateFormData(event) {
     const { name: key, value } = event.target;
-    setFormData({ ...formData, [key]: value })
+    setFormData({ ...formData, [key]: value });
   }
 
 
   async function sendForm(event) {
-    event.preventDefault()
-    await fetch('/api/petOwners/' + id, {
+    event.preventDefault();
+    await fetch('/api/pet-owners/' + documentId, {
       method: 'PUT',
       headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify(formData)
-    })
-    setFormSent(true)
+      body: JSON.stringify({ data: formData })
+    });
+    setFormSent(true);
   }
 
   if (formSent) {
@@ -56,7 +54,7 @@ export default function UpdatePetOwner() {
       <p>The pet owner {formData.name} has been updated</p>
       <button onClick={() => navigate('/pets-and-owners')}>
         Return to the list of pets and their owners</button>
-    </>
+    </>;
 
   } else {
 
@@ -64,8 +62,12 @@ export default function UpdatePetOwner() {
       <h2>Edit {petOwner.name}</h2>
       <form onSubmit={sendForm}>
         <label>
-          Name:
-          <input name="name" type="text" placeholder="Name" value={formData.name} onChange={updateFormData} />
+          First name:
+          <input name="firstName" type="text" placeholder="First name" value={formData.firstName} onChange={updateFormData} />
+        </label>
+        <label>
+          Last name:
+          <input name="lastName" type="text" placeholder="Last name" value={formData.lastName} onChange={updateFormData} />
         </label>
         <label>
           Email:
@@ -73,7 +75,7 @@ export default function UpdatePetOwner() {
         </label>
         <button type="submit">Update</button>
       </form>
-    </>
+    </>;
 
   }
 
